805bbf8d2e2a18863f10e4aa5c234f9eaa429c88
nandu44-code <nanduganeshnair4444@gmail.com>
2024-01-06 09:56:46 +0530

    apiSlice and authSlice created

---
+++ frontend/src/app/api/apiSlice.js
@@ -0,0 +1,36 @@
+import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+import { setCredentials,logOut } from '../../features/auth.authSlice'
+
+const baseQuery = fetchBaseQuery({
+    baseUrl: 'http://locathost:3500',
+    credentials: 'include',
+    prepareHeaders:(headers, {getState}) =>{
+        const token = getState().auth.token
+        if (token){
+            headers.set("authouization" , `Bearer ${token}`)
+        }
+        return headers
+    }
+})
+
+const baseQueryWithauth = async(args, api, extraOptions) => {
+    let result = await baseQuery(args,api, extraOptions)
+
+    if (result?.error?.originalstatus === 403){
+        console.log('sending refresh token')
+        //send refresh token to get new access token
+        const refreshResult = await baseQuery('/refresh', api, extraOptions)
+        console.log(refreshResult)
+
+        if (refreshResult?.data) {
+            const user = api.getState().auth.user
+            //store the new token 
+            api.dispatch (setCredentials({...refreshResult.data, user}))
+            //retry the original query with new access token
+            result = await baseQuery(args,api,extraOptions)
+        }else{
+            api.dispatch(logOut())
+        }
+    }
+    return result
+} 
